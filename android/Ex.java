6c8a77b44a6033e953cd24013cca2e6faef609b4
henshoef <36810075+henshoef@users.noreply.github.com>
2018-02-25 11:56:42 +0200

    Solution3

--- android/Ex.java
+++ android/Ex.java
@@ -5,6 +5,8 @@ public static void main(String[] args) {
 	Address address=new Address("Haalon", 52, "Kefar yona", "Israel");
 	Student hen=new Student(100, "JB", 23, "Hen Shoef", address);
 	Student ron=new Student(90, "JB", 21, "Ron Edri", address);
+	hen.setGrade(95);
+	System.out.println(hen.getGrade());
 	
 }
 }
--- android/Student.java
+++ android/Student.java
@@ -10,6 +10,22 @@ private	String schoolName;
 		this.schoolName=schoolName;
 		
 	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
 	
 
 }
