77ad93db41ebb4bfe971603fa9c91b545c2318c4
henshoef <36810075+henshoef@users.noreply.github.com>
2018-02-25 11:51:37 +0200

    Solution2

--- android/Ex.java
+++ android/Ex.java
@@ -2,6 +2,9 @@ package android;
 
 public class Ex {
 public static void main(String[] args) {
+	Address address=new Address("Haalon", 52, "Kefar yona", "Israel");
+	Student hen=new Student(100, "JB", 23, "Hen Shoef", address);
+	Student ron=new Student(90, "JB", 21, "Ron Edri", address);
 	
 }
 }
